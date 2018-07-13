c3e41b38d572c79d9a3c368512389bf11f6ce065
unknown <verystrongjoe@gmail.com>
2018-07-13 09:56:56 +0900

    temporary update

--- gym_core/tgym.py
+++ gym_core/tgym.py
@@ -22,31 +22,28 @@ C_HOME_FULL_DIR = config.GYM['HOME']
 class TradingGymEnv(Env):
 
     d_episodes_data = {}
+    l_err_occurred_idx = []
 
     # every agent has their own constraints to be managed by trading gym itself.
-    c_agent_max_num_of_allowed_transaction = 10
+    # c_agent_max_num_of_allowed_transaction = 10
     c_pickle_data_file = 'tgym-data.pickle'
 
-    # c_agent_prev_step_start_yyyymmdd_in_episode = '090501'  # 9hr 6min 1sec
-    # c_agent_step_start_yyyymmdd_in_episode = '090601' # 9hr 6min 1sec
+    c_step_start_yyyymmdd_in_episode = '090601'  # 09:06:01
+    c_prev_step_start_datetime_in_episode = None
+    c_step_start_datetime_in_episode = None
+    c_range_timestamp = []
 
-    c_agent_prev_step_start_datetime_in_episode = None
-    c_agent_step_start_datetime_in_episode = None
-
-    c_agent_range_timestamp = []
-
-    p_agent_current_num_transaction = 0
-    p_agent_current_episode_ref_idx = 0
-    p_agent_current_episode_data_quote = None
-    p_agent_current_episode_data_order = None
-    p_agent_current_episode_ticker = None
-    p_agent_current_episode_date = None
-    p_agent_current_episode_price_history = deque(maxlen=60)
-    p_agent_current_step_in_episode = 0 # step interval = 1sec
-    p_agent_max_num_of_allowed_transaction = 10
-
-    p_agent_is_stop_loss = None
-    p_agent_is_reached_goal = None
+    p_current_num_transaction = 0
+    p_current_episode_ref_idx = 0
+    p_current_episode_data_quote = None
+    p_current_episode_data_order = None
+    p_current_episode_ticker = None
+    p_current_episode_date = None
+    p_current_episode_price_history = deque(maxlen=60)
+    p_current_step_in_episode = 0 # step interval = 1sec
+    p_max_num_of_allowed_transaction = 10
+    p_is_stop_loss = None
+    p_is_reached_goal = None
 
     is_data_loaded = False
     episode_data_count = 0
@@ -82,11 +79,11 @@ class TradingGymEnv(Env):
 
         """
         # TODO : here we need to decide whether or not episode ends right way or wait until end of duration of original duration
-        if self.p_agent_current_num_transaction >= self.p_agent_max_num_of_allowed_transaction:
+        if self.p_current_num_transaction >= self.p_max_num_of_allowed_transaction:
             return True
-        elif self.p_agent_current_step_in_episode >= self.episode_duration_min * 60:
+        elif self.p_current_step_in_episode >= self.episode_duration_min * 60:
             return True
-        elif self.p_agent_is_stop_loss:
+        elif self.p_is_stop_loss:
             return False
         else:
             return False
@@ -219,7 +216,7 @@ class TradingGymEnv(Env):
             self.episode_data_count = self.create_episode_data(episode_type)
 
         # for now, episode type is not considered.
-        #self.p_agent_current_episode_ref_idx = random.randint(0, self.episode_data_count - 1)
+        #self.p_current_episode_ref_idx = random.randint(0, self.episode_data_count - 1)
 
         self.c_episode_max_step_count = 60 * episode_duration_min
 
@@ -234,15 +231,15 @@ class TradingGymEnv(Env):
         self.ob_transform = obs_transform
         self.episode_duration_min = episode_duration_min
 
-        p_agent_current_episode_price_history = deque(maxlen=self.episode_duration_min)
+        p_current_episode_price_history = deque(maxlen=self.episode_duration_min)
 
-        self.p_agent_current_step_in_episode = 0
-        self.p_agent_max_num_of_allowed_transaction = max_num_of_transaction
-        self.p_agent_is_stop_loss_price = 0
+        self.p_current_step_in_episode = 0
+        self.p_max_num_of_allowed_transaction = max_num_of_transaction
+        self.p_is_stop_loss_price = 0
 
 
-    def init_observation(self):
-        return self._get_observation()
+    # def init_observation(self):
+    #     return self._get_observation()
 
     def _rewards(self, observation, action, done, info):
         """
@@ -260,11 +257,11 @@ class TradingGymEnv(Env):
         2. open high close low based on step_interval in env
         3. price history for 1 minute
         """
-        p0 = self.d_episodes_data[self.p_agent_current_episode_ref_idx]['quote'].loc[self.c_agent_range_timestamp[self.p_agent_current_step_in_episode]]
-        p1 = self.d_episodes_data[self.p_agent_current_episode_ref_idx]['order'].loc[self.c_agent_range_timestamp[self.p_agent_current_step_in_episode]]
-        p2 = self.p_agent_current_episode_price_history
+        p0 = self.d_episodes_data[self.p_current_episode_ref_idx]['quote'].loc[self.c_range_timestamp[self.p_current_step_in_episode]]
+        p1 = self.d_episodes_data[self.p_current_episode_ref_idx]['order'].loc[self.c_range_timestamp[self.p_current_step_in_episode]]
+        p2 = self.p_current_episode_price_history
 
-        return np.append(np.append(p0, p1), p2)
+        return np.append(np.append(p0, p1), p2[-1])
 
     def step(self, action):
         """
@@ -272,7 +269,11 @@ class TradingGymEnv(Env):
         _get_observation needs to be transformed using transform observation that __init__ received.
 
         Args:
-            action (object): for now, action is simple! 1 or -1 ( buy all or sell all )
+            action (object):
+                1  : buy a stock with all money I have
+                -1 : sell every stock I have
+
+
         Returns:
             observation (object): agent's observation of the current environment
             reward (float) : amount of reward returned after previous action
@@ -280,19 +281,16 @@ class TradingGymEnv(Env):
             info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
         """
         if action == 0:
-            self.p_agent_current_step_in_episode = self.p_agent_current_step_in_episode + 1
-            next_base_price = self.p_agent_current_episode_data_order.loc[
-                self.c_agent_range_timestamp[self.p_agent_current_step_in_episode]]['SellHoga1']
+
+            self.p_current_step_in_episode += 1
+            next_base_price = self.p_current_episode_data_order.loc[
+                self.c_range_timestamp[self.p_current_step_in_episode]]['SellHoga1']
+
             if next_base_price <= -100:
                 can_buy = False
             else:
                 can_buy = True
 
-            # _info = [{'stop_loss': False,
-            #             'stop_loss_price': -1,
-            #             'reached_profit': False,
-            #             'best_price': -1,
-            #             'can_buy': can_buy}]
             _info = {}
             _info['stop_loss'] = False
             _info['stop_loss_price'] = -1
@@ -306,64 +304,64 @@ class TradingGymEnv(Env):
             _done = self._is_done()
 
             return _observation, self._rewards(_observation, action, _done, _info), _done, _info
-        else :
 
+        else:
             # base_price is the price that agent can buy the stock at now.
-            base_price = self.p_agent_current_episode_data_order.loc[
-                self.c_agent_range_timestamp[self.p_agent_current_step_in_episode]]['SellHoga1']
+            base_price = self.p_current_episode_data_order.loc[
+                self.c_range_timestamp[self.p_current_step_in_episode]]['SellHoga1']
 
             best_price = -10000000000
-            self.p_agent_is_stop_loss = False
-            self.p_agent_is_reached_goal = False
+            self.p_is_stop_loss = False
+            self.p_is_reached_goal = False
             buy_price = -1
             last_price = -1
 
             length = len(pd.date_range(
-                    self.c_agent_range_timestamp[self.p_agent_current_step_in_episode],
-                    self.c_agent_range_timestamp[
-                        np.minimum(self.p_agent_current_step_in_episode+60, self.c_episode_max_step_count-1)], freq='S'
+                    self.c_range_timestamp[self.p_current_step_in_episode],
+                    self.c_range_timestamp[
+                        np.minimum(self.p_current_step_in_episode + 60, self.c_episode_max_step_count - 1)], freq='S'
             ))
 
             for idx, present_ts in enumerate(pd.date_range(
-                    self.c_agent_range_timestamp[self.p_agent_current_step_in_episode],
-                    self.c_agent_range_timestamp[
-                        np.minimum(self.p_agent_current_step_in_episode+60, self.c_episode_max_step_count-1)], freq='S'
+                    self.c_range_timestamp[self.p_current_step_in_episode],
+                    self.c_range_timestamp[
+                        np.minimum(self.p_current_step_in_episode + 60, self.c_episode_max_step_count - 1)], freq='S'
 
             )):
 
-                present_price = self.p_agent_current_episode_data_order.loc[present_ts]['BuyHoga1']
+                present_price = self.p_current_episode_data_order.loc[present_ts]['BuyHoga1']
 
                 percent = ((present_price+100) - (base_price+100)) / ( base_price+100) * 100
 
                 if idx == 0:
-                    buy_price = self.p_agent_current_episode_data_order.loc[present_ts]['SellHoga1']
+                    buy_price = self.p_current_episode_data_order.loc[present_ts]['SellHoga1']
                 if idx == length - 1:  # if you change the window size, you must change it.
-                    last_price = self.p_agent_current_episode_data_order.loc[present_ts]['BuyHoga1']
+                    last_price = self.p_current_episode_data_order.loc[present_ts]['BuyHoga1']
 
-                if not self.p_agent_is_reached_goal and percent < 0 and self.percent_stop_loss <= np.abs(percent):
-                    self.p_agent_is_stop_loss = True
-                    self.p_agent_is_stop_loss_price = present_price  # TODO: ASK1 is correct price for stop loss ?!
+                if not self.p_is_reached_goal and percent < 0 and self.percent_stop_loss <= np.abs(percent):
+                    self.p_is_stop_loss = True
+                    self.p_is_stop_loss_price = present_price  # TODO: ASK1 is correct price for stop loss ?!
                     break
                 elif percent > 0 and self.percent_goal_profit <= np.abs(percent):
-                    self.p_agent_is_reached_goal = True
+                    self.p_is_reached_goal = True
                     if best_price < present_price:
                         best_price = present_price
                 else:
                     pass
 
             # adding one step after all processes are done.
-            self.p_agent_current_step_in_episode = self.p_agent_current_step_in_episode + 1
-            next_base_price = self.p_agent_current_episode_data_order.loc[
-                self.c_agent_range_timestamp[self.p_agent_current_step_in_episode]]['SellHoga1']
+            self.p_current_step_in_episode = self.p_current_step_in_episode + 1
+            next_base_price = self.p_current_episode_data_order.loc[
+                self.c_range_timestamp[self.p_current_step_in_episode]]['SellHoga1']
             if next_base_price <= -100:
                 can_buy = False
             else:
                 can_buy = True
 
             _info = {}
-            _info['stop_loss'] = self.p_agent_is_stop_loss
-            _info['stop_loss_price'] = self.p_agent_is_stop_loss_price
-            _info['reached_profit'] = self.p_agent_is_reached_goal
+            _info['stop_loss'] = self.p_is_stop_loss
+            _info['stop_loss_price'] = self.p_is_stop_loss_price
+            _info['reached_profit'] = self.p_is_reached_goal
             _info['best_price'] = best_price
             _info['can_buy'] = can_buy
             _info['buy_price'] = buy_price
@@ -376,49 +374,55 @@ class TradingGymEnv(Env):
 
 
     def reset(self):
-        """Reset the state of the environment and returns an initial observation.
+        """
+        Reset the state of the environment and returns an initial observation.
 
-        here we will reset agent's enviroment to restart.
+        here we will reset agent's environment to restart.
         for now, we need to have its database to load and the status will go back to its first status like it called init
 
-        everytime, it needs to reset index to reference which index of episode data agent use training data.
+        every time, it needs to reset index to reference which index of episode data agent use training data.
 
         Returns:
             numpy.array: The initial observation of the space. Initial reward is assumed to be 0.
         """
-        self.p_agent_current_episode_ref_idx = random.randint(0, self.episode_data_count-1)
-        self.p_agent_current_step_in_episode = 0
+        # TODO : I can't not remember why I think it needs error exception here.
+        try:
+            self.p_current_episode_ref_idx = random.randint(0, self.episode_data_count - 1)
+            self.p_current_step_in_episode = 0
 
-        self.p_agent_current_episode_ticker = self.d_episodes_data[self.p_agent_current_episode_ref_idx]['meta']['ticker']
-        self.p_agent_current_episode_date = self.d_episodes_data[self.p_agent_current_episode_ref_idx]['meta']['date']
-        self.p_agent_current_episode_data_quote = self.d_episodes_data[self.p_agent_current_episode_ref_idx]['quote']
-        self.p_agent_current_episode_data_order = self.d_episodes_data[self.p_agent_current_episode_ref_idx]['order']
+            self.p_current_episode_ticker = self.d_episodes_data[self.p_current_episode_ref_idx]['meta']['ticker']
+            self.p_current_episode_date = self.d_episodes_data[self.p_current_episode_ref_idx]['meta']['date']
+            self.p_current_episode_data_quote = self.d_episodes_data[self.p_current_episode_ref_idx]['quote']
+            self.p_current_episode_data_order = self.d_episodes_data[self.p_current_episode_ref_idx]['order']
 
-        current_date = self.p_agent_current_episode_date
+            current_date = self.p_current_episode_date
 
-        print(self.p_agent_current_episode_ticker)
-        print(self.p_agent_current_episode_date)
+            print(self.p_current_episode_ticker)
+            print(self.p_current_episode_date)
 
-        # it can not declared in init method. because we need to consider yyyymmdd in future.
-        self.c_agent_prev_step_start_datetime_in_episode = datetime.datetime(int(current_date[0:4]),
-                                                                             int(current_date[4:6]),
-                                                                             int(current_date[6:8]), 9, 5)
-        self.c_agent_step_start_datetime_in_episode = datetime.datetime(int(current_date[0:4]),
-                                                                        int(current_date[4:6]),
-                                                                        int(current_date[6:8]), 9, 6)
+            # it can not declared in init method. because we need to consider yyyymmdd in future.
+            self.c_prev_step_start_datetime_in_episode = datetime.datetime(int(current_date[0:4]),
+                                                                           int(current_date[4:6]),
+                                                                           int(current_date[6:8]), 9, 5)
+            self.c_step_start_datetime_in_episode = datetime.datetime(int(current_date[0:4]),
+                                                                      int(current_date[4:6]),
+                                                                      int(current_date[6:8]), 9, 6)
 
-        self.c_agent_range_timestamp = pd.date_range(
-            self.c_agent_step_start_datetime_in_episode, periods=self.c_episode_max_step_count+1, freq='S')
+            self.c_range_timestamp = pd.date_range(
+                self.c_step_start_datetime_in_episode, periods=self.c_episode_max_step_count + 1, freq='S')
 
-        start = datetime.datetime(int(current_date[0:4]), int(current_date[4:6]), int(current_date[6:8]), 9, 5)
-        prev_read_rng = \
-            pd.date_range(start, periods=np.minimum(
-                self.c_episode_max_step_count-self.p_agent_current_step_in_episode,
-                self.episode_duration_min*60), freq='S')
+            start = datetime.datetime(int(current_date[0:4]), int(current_date[4:6]), int(current_date[6:8]), 9, 5)
+            prev_read_rng = \
+                pd.date_range(start, periods=np.minimum(
+                    self.c_episode_max_step_count-self.p_current_step_in_episode,
+                    self.episode_duration_min*60), freq='S')
 
-        for prev_time_step in prev_read_rng:
-            self.p_agent_current_episode_price_history.append(
-                    self.p_agent_current_episode_data_quote.loc[prev_time_step]['Price(last excuted)'])
+            for prev_time_step in prev_read_rng:
+                self.p_current_episode_price_history.append(
+                        self.p_current_episode_data_quote.loc[prev_time_step]['Price(last excuted)'])
+        except Exception:
+            print('we get error')
+
 
         return self._get_observation()
 
@@ -431,7 +435,5 @@ class TradingGymEnv(Env):
 
     def _get_status(self):
         logs = {}
-        logs['agent_current_num_transaction'] = self.p_agent_current_num_transaction
+        logs['current_num_transaction'] = self.p_current_num_transaction
         return logs
-
-
